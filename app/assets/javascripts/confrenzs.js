67621b8325c85d8e602400e663b2a108a19c582f
krzysztofwolowicz <wolowicz.krzysztof@gmail.com>
2017-03-09 11:24:50 +0100

    jquery

---
+++ app/assets/javascripts/confrenzs.js
@@ -0,0 +1,151 @@
+$(document).on('turbolinks:load', function(){
+    $( ".sortable" ).sortable();
+    $( ".sortable" ).disableSelection();
+  } );
+
+//   $( function() {
+//     var availableTags = [
+//       "ActionScript",
+//       "AppleScript",
+//       "Asp",
+//       "BASIC",
+//       "C",
+//       "C++",
+//       "Clojure",
+//       "COBOL",
+//       "ColdFusion",
+//       "Erlang",
+//       "Fortran",
+//       "Groovy",
+//       "Haskell",
+//       "Java",
+//       "JavaScript",
+//       "Lisp",
+//       "Perl",
+//       "PHP",
+//       "Python",
+//       "Ruby",
+//       "Scala",
+//       "Scheme"
+//     ];
+//     $( "#tags" ).autocomplete({
+//       source: availableTags
+//     });
+//   } );
+//
+//   $( function() {
+//     $( "#datepicker" ).datepicker();
+//   } );
+//
+//
+// $(document).ready(function(){
+//     $("#flip").click(function(){
+//         $("#panel").slideToggle("slow");
+//     });
+// });
+
+$(document).on('turbolinks:load', function(){
+
+    $(".link1").click(function(){
+        $(".link-1").slideToggle("slow");
+    });
+});
+
+$(document).on('turbolinks:load', function(){
+
+    $(".link2").click(function(){
+        $(".link-2").slideToggle("slow");
+    });
+});
+
+$(document).on('turbolinks:load', function(){
+    $(".link3").click(function(){
+        $(".link-3").slideToggle("slow");
+    });
+});
+
+$(document).on('turbolinks:load', function(){
+    $(".link4").click(function(){
+        $(".link-4").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link5").click(function(){
+        $(".link-5").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link6").click(function(){
+        $(".link-6").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link7").click(function(){
+        $(".link-7").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link8").click(function(){
+        $(".link-8").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link9").click(function(){
+        $(".link-9").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link10").click(function(){
+        $(".link-10").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link11").click(function(){
+        $(".link-11").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link12").click(function(){
+        $(".link-12").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link13").click(function(){
+        $(".link-13").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link14").click(function(){
+        $(".link-14").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link15").click(function(){
+        $(".link-15").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link16").click(function(){
+        $(".link-16").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link17").click(function(){
+        $(".link-17").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link18").click(function(){
+        $(".link-18").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link19").click(function(){
+        $(".link-19").slideToggle("slow");
+    });
+});
+$(document).on('turbolinks:load', function(){
+    $(".link20").click(function(){
+        $(".link-20").slideToggle("slow");
+    });
+});
--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,4 +1,14 @@
 class SubjectsController < ApplicationController
+  before_action :set_confrenz
+
+  def index
+
+  end
+
+  def agendas
+    
+  end
+
   def create
     @confrenz = Confrenz.find(params[:confrenz_id])
     @subject = @confrenz.subjects.create(params[:subject].permit(:name, :hour))
@@ -9,5 +19,11 @@ class SubjectsController < ApplicationController
     @subject = @confrenz.subjects.find(params[:id])
     @subject.destroy
     redirect_to confrenz_path(@confrenz)
+  end
+
+  private
+
+  def set_confrenz
+    @confrenz = Confrenz.find(params[:confrenz_id])
   end
 end
